1ee0d73834225361ddfad4cb29692da3f7fe7e61
Gabriel <gabriel.lopesblue13@gmail.com>
2025-03-20 21:04:20 -0300

    banco de dados

--- _js/script.js
+++ _js/script.js
@@ -7,8 +7,26 @@ function irParaOutraPagina() {
 }
 
 
+//Nao serve pra nada, apagar dps
+
 function voltarPagina() {
         window.location.href = "index.html"; // Altere para a página desejada
     }
 
 
+function handleLogin() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !email || !password) {
+        alert("Please fill in all fields!");
+        return;
+    }
+
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    alert("Login successful!"); // Simula o login
+
+    }
